28c8adb99b01f1668ac44ec5c18536e96254804d
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-08 09:27:46 -0700

    start 🚀🚀

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+   {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      image: 'https://images.unsplash.com/photo-1601288496920-b6154fe3626a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+   },
+   {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      image: 'https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZmFjZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+   },
+   {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZhY2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+   },
+   {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZhY2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+   },
+];
+
+export default reviews;
